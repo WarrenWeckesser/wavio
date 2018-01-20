05779aa3f2724a6ba518197e5e67c5269dcda2f3
Warren Weckesser <warren.weckesser@gmail.com>
2018-01-19 22:25:48 -0500

    MAINT: Remove extraneous parentheses.

--- wavio.py
+++ wavio.py
@@ -201,7 +201,7 @@ def _scale_to_sampwidth(data, sampwidth, vmin, vmax):
         if outmin != vmin or outmax != vmax:
             vmin = float(vmin)
             vmax = float(vmax)
-            data = ((float(outmax - outmin)) * (data - vmin) /
+            data = (float(outmax - outmin) * (data - vmin) /
                     (vmax - vmin)).astype(_np.int64) + outmin
             data[data == outmax] = outmax - 1
         data = data.astype(dt)
