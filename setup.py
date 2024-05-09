7b9fabb99cc5abbd3ceefa3614c95c1813764785
warren <warren.weckesser@gmail.com>
2024-05-08 22:13:26 -0400

    MAINT: Update Python version list in setup metadata.

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     keywords="wav numpy",
     py_modules=["wavio"],
